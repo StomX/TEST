f60d3c0cb120b37acb5ad5155495e4c80217c030
Jaymon <jaymon@bookips.com>
2021-04-13 12:46:24 +0900

    init

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView';
+import AskView from '../views/AskView';
+import JobsView from '../views/JobsView';
+import ItemView from '../views/ItemView';
+import UserView from '../views/UserView';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/news',
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/news',
+            component: NewsView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+    ],
+    mode: 'history'
+});
---
+++ src/views/NewsView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <ul v-if="users.length > 0">
+          <!-- header -->
+          <!-- // header -->
+          <li v-for="user in users">
+              <span v-text="user.title"></span>
+              {{user}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        .then(Response => this.users = Response.data)
+        .catch(error => console.error(error))
+    }
+}
+</script>
+
+<style>
+
+</style>
